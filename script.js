9142159259b00c0022ff485ed73ce39ee82ac29b
AlexM9715 <alexmtz9715@gmail.com>
2022-06-07 22:19:40 -0700

    Added Font

--- script.js
+++
@@ -1,8 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
-
-var typed = new Typed(".auto-type", {
-    strings: ["Software Developer", "Full-Stack Developer", "Photgrapher"],
-    typeSpeed: 150,
-    backSpeed: 150,
-    loop: true
-})
